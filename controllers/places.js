729545d48ef3fdfae141cbcb3e5d01fd73bf658e
CodyCork <codycork.sfg@outlook.com>
2022-07-28 21:07:59 -0500

    Form data is getting through!

--- controllers/places.js
+++ controllers/places.js
@@ -1,27 +1,33 @@
-const router = require('express').Router()
+const router = require("express").Router();
 
-router.get('/new', (req, res) => {
-  
-  res.render('places/new')
-})
+router.post("/", (req, res) => {
+  console.log(req.body);
+  res.send("POST /places");
+});
 
+router.get("/new", (req, res) => {
+  res.render("places/new");
+});
 
-router.get('/', (req, res) => {
-    let places = [{
-        name: 'H-Thai-ML',
-        city: 'Seattle',
-        state: 'WA',
-        cuisine: 'Thai, Pan-Asian',
-        pic: 'css/images/thaiNoodles.jpg'
-      }, {
-        name: 'Coding Cat Cafe',
-        city: 'Phoenix',
-        state: 'AZ',
-        cuisine: 'Coffee, Bakery',
-        pic: 'css/images/pastry.jpg'
-      }]
-      
-    res.render('places/index', { places })
-})
+router.get("/", (req, res) => {
+  let places = [
+    {
+      name: "H-Thai-ML",
+      city: "Seattle",
+      state: "WA",
+      cuisine: "Thai, Pan-Asian",
+      pic: "css/images/thaiNoodles.jpg",
+    },
+    {
+      name: "Coding Cat Cafe",
+      city: "Phoenix",
+      state: "AZ",
+      cuisine: "Coffee, Bakery",
+      pic: "css/images/pastry.jpg",
+    },
+  ];
 
-module.exports = router
+  res.render("places/index", { places });
+});
+
+module.exports = router;
