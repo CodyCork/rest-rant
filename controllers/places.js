2c5f59115c0d611dcaa3e9a4bc1ae4e43482ccef
CodyCork <codycork.sfg@outlook.com>
2022-07-28 21:46:12 -0500

    New places can be added

--- controllers/places.js
+++ controllers/places.js
@@ -1,8 +1,20 @@
 const router = require("express").Router();
+const places = require("../models/places.js");
 
 router.post("/", (req, res) => {
-  console.log(req.body);
-  res.send("POST /places");
+  // console.log(req.body);
+  if (!req.body.pic) {
+    // Default image if one is not provided
+    req.body.pic = "http://placekitten.com/400/400";
+  }
+  if (!req.body.city) {
+    req.body.city = "Anytown";
+  }
+  if (!req.body.state) {
+    req.body.state = "USA";
+  }
+  places.push(req.body);
+  res.redirect("/places");
 });
 
 router.get("/new", (req, res) => {
@@ -10,22 +22,22 @@ router.get("/new", (req, res) => {
 });
 
 router.get("/", (req, res) => {
-  let places = [
-    {
-      name: "H-Thai-ML",
-      city: "Seattle",
-      state: "WA",
-      cuisine: "Thai, Pan-Asian",
-      pic: "css/images/thaiNoodles.jpg",
-    },
-    {
-      name: "Coding Cat Cafe",
-      city: "Phoenix",
-      state: "AZ",
-      cuisine: "Coffee, Bakery",
-      pic: "css/images/pastry.jpg",
-    },
-  ];
+  // let places = [
+  //   {
+  //     name: "H-Thai-ML",
+  //     city: "Seattle",
+  //     state: "WA",
+  //     cuisine: "Thai, Pan-Asian",
+  //     pic: "css/images/thaiNoodles.jpg",
+  //   },
+  //   {
+  //     name: "Coding Cat Cafe",
+  //     city: "Phoenix",
+  //     state: "AZ",
+  //     cuisine: "Coffee, Bakery",
+  //     pic: "css/images/pastry.jpg",
+  //   },
+  // ];
 
   res.render("places/index", { places });
 });
