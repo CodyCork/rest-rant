a628632b23ac333db66097c452cca7d25ddc822e
CodyCork <codycork.sfg@outlook.com>
2022-07-29 21:17:05 -0500

    Delete functionality not ready

--- views/places/show.jsx
+++ views/places/show.jsx
@@ -1,14 +1,31 @@
-const React = require('react')
-const Def = require('../default')
+const React = require("react");
+const Def = require("../default");
 
-function show () {
-    return (
-        <Def>
-          <main>
-            <h1>Show Page</h1>
-          </main>
-        </Def>
-    )
+function show(data) {
+  return (
+    <Def>
+      <main>
+        <img src={place.pic} alt={place.name} />
+        <div>
+          <h1>{data.place.name}</h1>
+          <h2>Rating</h2>
+          <p>Not Rated</p>
+          <h2>Description</h2>
+          <p>
+            Located in {place.city}, {place.state} and serving {place.cuisine}
+          </p>
+        </div>
+        <a href={`/places/${data.id}/edit`} className="btn btn-warning">
+          Edit
+        </a>
+        <form method="POST" action={`/places/${data.id}?_method=DELETE`}>
+          <button type="submit" className="btn btn-danger">
+            Delete
+          </button>
+        </form>
+      </main>
+    </Def>
+  );
 }
 
-module.exports = show
+module.exports = show;
